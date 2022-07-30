0ddd95210a838b63d415f268c2291a1c25e95af8
Lidia Popdimitrova <104692293+lidiapopdimitrova@users.noreply.github.com>
2022-07-30 15:17:49 +0300

    Add files via upload

---
+++ polimorphism_and_abstraction_lab/run_time_polimorphism.py
@@ -0,0 +1,18 @@
+class Shape:
+    def calculate_area(self):
+        return None
+
+
+class Square(Shape):
+    side_length = 2
+
+    def calculate_area(self):
+        return self.side_length * 2
+
+
+class Triangle(Shape):
+    base_length = 4
+    height = 3
+
+    def calculate_area(self):
+        return 0.5 * self.base_length * self.height
