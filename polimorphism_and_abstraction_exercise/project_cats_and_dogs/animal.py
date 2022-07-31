1c2b30bd3efc57eea7c67dd712e15f636c3611bb
Lidia Popdimitrova <104692293+lidiapopdimitrova@users.noreply.github.com>
2022-07-31 13:39:58 +0300

    Add files via upload

---
+++ polimorphism_and_abstraction_exercise/project_cats_and_dogs/animal.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+
+
+class Animal(ABC):
+    def __init__(self, name, age, gender):
+        self.gender = gender
+        self.age = age
+        self.name = name
+
+    @abstractmethod
+    def __repr__(self):
+        pass
+
+    @abstractmethod
+    def make_sound(self):
+        pass
+
---
+++ polimorphism_and_abstraction_exercise/project_cats_and_dogs/kitten.py
@@ -0,0 +1,14 @@
+from polimorphism_and_abstraction_exercise.project_cats_and_dogs.cat import Cat
+
+
+class Kitten(Cat):
+    def __repr__(self):
+        return f"This is {self.name}. {self.name} is a {self.age} year old {self.gender} {self.__class__.__name__}"
+
+    def make_sound(self):
+        return 'Meow'
+
+    def __init__(self, name, age):
+        super().__init__(name, age, gender='Female')
+
+
