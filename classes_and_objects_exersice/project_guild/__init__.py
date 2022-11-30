258a25c44f896005db05b1cf5d791af1a8bfbe1e
Lidia Popdimitrova <104692293+lidiapopdimitrova@users.noreply.github.com>
2022-11-30 23:38:57 +0200

    Delete classes_and_objects_exersice directory

--- classes_and_objects_exersice/project_guild/__init__.py
+++
@@ -1,9 +0,0 @@
-from project_guild.guild import Guild
-from project_guild.player import Player
-
-player = Player("George", 50, 100)
-print(player.add_skill("Shield Break", 20))
-print(player.player_info())
-guild = Guild("UGT")
-print(guild.assign_player(player))
-print(guild.guild_info())
