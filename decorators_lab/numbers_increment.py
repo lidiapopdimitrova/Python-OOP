8acc93587fab8c48e4914160d9c40566f813e88f
Lidia Popdimitrova <104692293+lidiapopdimitrova@users.noreply.github.com>
2022-12-07 23:38:56 +0200

    Delete decorators_lab directory

--- decorators_lab/numbers_increment.py
+++
@@ -1,8 +0,0 @@
-def number_increment(numbers):
-    def increase():
-        increased = [x + 1 for x in numbers]
-        return increased
-    return increase()
-
-
-print(number_increment([1, 2, 3]))
