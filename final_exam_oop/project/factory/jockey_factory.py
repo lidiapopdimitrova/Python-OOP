e46e9c86e443aca548dd44c93028dac43092ac47
Lidia Popdimitrova <104692293+lidiapopdimitrova@users.noreply.github.com>
2023-01-12 20:30:04 +0200

    Delete final_exam_oop directory

--- final_exam_oop/project/factory/jockey_factory.py
+++
@@ -1,7 +0,0 @@
-from project.jockey import Jockey
-
-
-class JockeyFactory:
-    @staticmethod
-    def create_a_jockey(jockey_name, jockey_age):
-        return Jockey(jockey_name, jockey_age)
--- final_exam_oop/project/factory/race_factory.py
+++
@@ -1,7 +0,0 @@
-from project.horse_race import HorseRace
-
-
-class RaceFactory:
-    @staticmethod
-    def create_race(race_type):
-        return HorseRace(race_type)
